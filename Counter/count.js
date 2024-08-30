0e57644ae324fd71334604537cf953fcdcfdbbd1
Ravikumar Jha <jharavi153@gmail.com>
2024-08-30 17:30:30 +0530

    first  Change

---
+++ Counter/count.js
@@ -0,0 +1,4 @@
+const count = document.querySelector("#count");
+const sub = document.querySelector("#sub");
+const add = document.querySelector("#add");
+const reset = document.querySelector("#reset");
