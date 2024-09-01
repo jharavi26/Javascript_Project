3b77ada5c2ac29e28db07ff7bad1fc1e66d3ef1a
Ravikumar Jha <jharavi153@gmail.com>
2024-09-01 21:01:00 +0530

    final2 commit

--- Increment_Counter/style.js
+++ Increment_Counter/style.js
@@ -6,13 +6,13 @@ counters.forEach(counter => {
     counter.innerHTML = '0';
     let target = +counter.getAttribute('data-target');
     console.log(target)
-    let inc = target / 20;
+    let inc = target / 10;
     let count = 0;
     function incrementcounter(){
         if( count < target){
             count = count + inc;
             counter.innerHTML = count;
-            setInterval(incrementcounter, 1);
+            setInterval(incrementcounter, 100);
         }else{
             counter.innerHTML = target;
         }
